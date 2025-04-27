1c5d024f5160463f2819791282affa78289b224f
Abhilash Kumar <madiabhilash1563@gmail.com>
2025-04-28 02:05:59 +0530

    feat: client setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,14 +2,19 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	server: { port: 3000, open: true },
-	build: { outDir: resolve(__dirname, "../public") },
-	resolve: { alias: {} },
+	build: { outDir: resolve(__dirname, "../dist") },
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src")
+		}
+	},
 });
