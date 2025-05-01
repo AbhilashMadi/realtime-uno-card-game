227e02afe201a8c53dba55f65bc97c9ae4945c42
Abhilash Kumar <madiabhilash1563@gmail.com>
2025-05-02 01:09:42 +0530

    feat: enhance room forms with input fields and alerts for better user experience

--- client-vite/src/components/forms/join-room-form.tsx
+++ client-vite/src/components/forms/join-room-form.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
 import {
   Modal,
   ModalContent,
@@ -6,7 +7,10 @@ import {
   ModalBody,
   ModalFooter,
 } from "@heroui/modal";
-import { type FC } from "react";
+import { useState, type FC } from "react";
+import { Alert } from "@heroui/alert";
+
+import { EyeIcon } from "../icons";
 
 interface IJoinRoomForm {
   isOpen: boolean;
@@ -14,21 +18,49 @@ interface IJoinRoomForm {
 }
 
 const JoinRoomForm: FC<IJoinRoomForm> = ({ isOpen, onClose }) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const togglePasswordView = (): void => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <Modal backdrop={"opaque"} isOpen={isOpen} onClose={onClose}>
       <ModalContent>
-        <ModalHeader className="flex flex-col gap-1">Join Room</ModalHeader>
-        <ModalBody>
-          <form>Join Room</form>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="danger" variant="light" onPress={onClose}>
-            Cancel
-          </Button>
-          <Button color="primary" onPress={onClose}>
-            Join Now
-          </Button>
-        </ModalFooter>
+        <form>
+          <ModalHeader className="flex flex-col gap-1">Join Room</ModalHeader>
+          <ModalBody>
+            <Input
+              isClearable
+              isRequired
+              label="Room ID"
+              name="room_id"
+              placeholder="e.g., 2T240LGW"
+              variant="bordered"
+            />
+            <Input
+              endContent={
+                <button type="button" onClick={togglePasswordView}>
+                  <EyeIcon open={showPassword} />
+                </button>
+              }
+              label="Room Password"
+              name="room_password"
+              placeholder="Enter room password for access if it is private!"
+              type={showPassword ? "text" : "password"}
+              variant="bordered"
+            />
+            <Alert color="danger" />
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" variant="light" onPress={onClose}>
+              Cancel
+            </Button>
+            <Button color="primary" onPress={onClose}>
+              Join Now
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
